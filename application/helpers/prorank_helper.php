62c4996158d20896904431e75e0030d83e1d3aad
gikavchukandrey <31930739+gikavchukandrey@users.noreply.github.com>
2017-12-12 14:11:11 +0200

    Update prorank_helper.php

--- application/helpers/prorank_helper.php
+++ application/helpers/prorank_helper.php
@@ -51,7 +51,10 @@ function PR_validateDomain($url,$process,$timeout = 8)
 		$json['error'] = __("The domain name contains forbidden words");
 	}
 	else
-	{   print_r($domain_curl);
+	{  
+$domain_curl = substr($domain_curl,1);
+ print_r($domain_curl);
+
 		if(is_valid_domain_name($domain_curl))
 		{	
 			$ret = ping($domain_curl,$timeout);
