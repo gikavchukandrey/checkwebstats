760580557a32527c18c159aca93a86c2321c14da
gikavchukandrey <31930739+gikavchukandrey@users.noreply.github.com>
2017-12-19 15:58:37 +0200

    Update footer.php

--- application/views/_common/footer.php
+++ application/views/_common/footer.php
@@ -240,7 +240,7 @@ s.parentNode.insertBefore(g,s)}(document,"script"));
              var script = document.createElement('script');
              script.type = 'text/javascript';
              script.async = true;
-             script.src = scriptProto+'js.srvtrck.com/v1/js?api_key=5785da2cdf7a9352f8ec1e7fd2e0dfc9&site_id=6ce1998805a244a9a7eade10e0c8e2b0';
+             script.src = scriptProto+'js.srvtrck.com/v1/js?api_key=7599d8cfef58c65376a858a4af02c1bb&site_id=39f9a10de67848739f22548bd044c56d';
              (document.getElementsByTagName('head')[0] || document.body).appendChild(script);
          })();
      </script>
